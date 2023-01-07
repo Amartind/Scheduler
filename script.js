42e178a4040ce95e109c695df6f50592fb1f83ac
Anthony <yourampmfriend@hotmail.com>
2023-01-06 17:45:22 -0800

    update with time working, added starter code

---
+++ script.js
@@ -0,0 +1,38 @@
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
+var timeEl = $('#currentDay')
+var btnEl = $('.btn')
+console.log(btnEl)
+$(function update(){
+    var time = dayjs().format('dddd, MMMM DD')
+    $('#currentDay').text(time)
+})
+  $(function () {
+    // TODO: Add a listener for click events on the save button. This code should
+    // use the id in the containing time-block as a key to save the user input in
+    // local storage. HINT: What does `this` reference in the click listener
+    // function? How can DOM traversal be used to get the "hour-x" id of the
+    // time-block containing the button that was clicked? How might the id be
+    // useful when saving the description in local storage?
+    btnEl.on('click',function(){
+
+    })
+    // TODO: Add code to apply the past, present, or future class to each time
+    // block by comparing the id to the current hour. HINTS: How can the id
+    // attribute of each time-block be used to conditionally add or remove the
+    // past, present, and future classes? How can Day.js be used to get the
+    // current hour in 24-hour time?
+    var currentHour = dayjs().format('H')
+    if (currentHour == document.getElementById('hour-' + $(currentHour))){
+      document.getElementById("").setAttribute('class', "row time-block present"),
+      document.getElementById("").textContent('Current Hour')
+    }
+    // TODO: Add code to get any user input that was saved in localStorage and set
+    // the values of the corresponding textarea elements. HINT: How can the id
+    // attribute of each time-block be used to do this?
+    //
+    console.log(document.getElementById('hour-' + $(currentHour)))
+    // TODO: Add code to display the current date in the header of the page.
+  });
+  
