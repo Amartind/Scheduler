30ca9ead94f9fc3930138366c84323a0ce62d2a5
Anthony <yourampmfriend@hotmail.com>
2023-01-07 13:36:28 -0800

    updated JS : able to pull closest hour and starting input

--- script.js
+++ script.js
@@ -3,6 +3,9 @@
 // in the html.
 var timeEl = $('#currentDay')
 var btnEl = $('.btn')
+var timeOfDayEl = $('.hour')
+console.log(timeOfDayEl)
+
 console.log(btnEl)
 $(function update(){
     var time = dayjs().format('dddd, MMMM DD')
@@ -15,24 +18,38 @@ $(function update(){
     // function? How can DOM traversal be used to get the "hour-x" id of the
     // time-block containing the button that was clicked? How might the id be
     // useful when saving the description in local storage?
-    btnEl.on('click',function(){
-
-    })
+      btnEl.on('click',function(){
+      console.log(document.body.children[1].children);
+      console.log(btnEl.prevAll('div').children());
+      var timeBlockId = $(this).closest('.time-block').attr('id');
+      console.log(timeBlockId)
+      var userInput = $('.description').val();
+      console.log(userInput)
+      // if (currentHour === btnEl.prevAll('div').html()){
+      //   console.log('hello')
+      // };
+})
     // TODO: Add code to apply the past, present, or future class to each time
     // block by comparing the id to the current hour. HINTS: How can the id
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
-    var currentHour = dayjs().format('H')
-    if (currentHour == document.getElementById('hour-' + $(currentHour))){
-      document.getElementById("").setAttribute('class', "row time-block present"),
-      document.getElementById("").textContent('Current Hour')
-    }
+      var currentHour = dayjs().format('H')
+console.log(this)
+      if (currentHour === timeBlockId){
+      
+        array.forEach(currentHour === {
+          
+        });
+      }
+      removeClass("past");
+      setClass("present");
+      textContent('Current Hour')
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
-    //
-    console.log(document.getElementById('hour-' + $(currentHour)))
+
+      console.log(currentHour)
     // TODO: Add code to display the current date in the header of the page.
   });
   
